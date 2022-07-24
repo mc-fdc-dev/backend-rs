9f68f637bd49f65dded47a30795f3ed2c87a6734
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-24 12:29:13 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use actix_web::{get, App, HttpServer, HttpResponse, Responder};
+use actix_web::middleware::Logger;
+use env_logger::Env;
 
 
 #[get("/")]
@@ -8,9 +10,12 @@ async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
     HttpServer::new(|| {
         App::new()
             .service(hello)
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
     })
     .bind(("0.0.0.0", 8080))?
     .run()
