97031229a09d038c9e49288f9bb645eb5b332b50
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-24 12:25:08 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, Responder};
+
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
